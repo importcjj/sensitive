fc1b5a3856242e583830e61f35a2dba286b9fb4d
Importcjj <importcjj@gmail.com>
2017-04-01 14:52:46 +0800

    Add examples

---
+++ examples/simple.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"github.com/importcjj/sensitive"
+)
+
+func main() {
+	filter := sensitive.New()
+	filter.LoadWordDict("dict.txt")
+	filter.AddWord("长者")
+
+	fmt.Println(filter.Filter("我为长者续一秒")) // 我为续一秒
+	// 42 即 "*"
+	fmt.Println(filter.Replace("我为长者续一秒", 42)) // 我为**续一秒
+	fmt.Println(filter.FindIn("我为长者续一秒"))      // true, 长者
+
+	fmt.Println(filter.FindIn("我为长|者续一秒")) // false,
+	filter.UpdateNoisePattern(`\|`)
+	fmt.Println(filter.FindIn("我为长|者续一秒")) // true, 长者
+}

702162dee0aa75b337062b35ffc9f3ee2740a165
importcjj <import@gmail.com>
2018-07-29 15:34:34 +0800

    Fix FindIn and Update README.md

---
+++ examples/readme/main.go
@@ -0,0 +1,25 @@
+
+package main
+
+import (
+	"fmt"
+	"github.com/importcjj/sensitive"
+)
+
+func main() {
+	filter := sensitive.New()
+	filter.LoadWordDict("../dict/dict.txt")
+	filter.AddWord("长者")
+
+	fmt.Println(filter.Filter("我为长者续一秒")) // 我为续一秒
+	// 42 即 "*"
+	fmt.Println(filter.Replace("我为长者续一秒", 42)) // 我为**续一秒
+	fmt.Println(filter.FindIn("我为长者续一秒"))      // true, 长者
+	fmt.Println(filter.Validate("我为长者续一秒"))      // False, 长者
+	fmt.Println(filter.FindAll("我为长者续一秒")) // [长者]
+
+	fmt.Println(filter.FindIn("我为长x者续一秒")) // false
+	filter.UpdateNoisePattern(`x`)
+	fmt.Println(filter.FindIn("我为长x者续一秒")) // true, 长者
+	fmt.Println(filter.Validate("我为长x者续一秒"))      // False, 长者
+}
---
+++ examples/test_issue_3/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"github.com/importcjj/sensitive"
+)
+
+func keywordFilterSearch(content string) (bool, string) {
+	filter := sensitive.New()
+	filter.AddWord("hello")
+	// filter.LoadWordDict("./conf/dict.txt")
+	code, word := filter.FindIn(content)
+	fmt.Println(content)
+	fmt.Printf("[%s]\n",word)
+	fmt.Printf("[%v]\n",code)
+	return code, word
+}
+
+func main() {
+	content := `录制 文字 吃猪肉
+sdaddasd
+dadasd
+dada
+教性
+`
+	keywordFilterSearch(content)
+}
